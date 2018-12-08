85925a4002b82d98536764e81f80ba8a8c9f3ef6
Evan Harry <evan.harry@apg-aarl.co.uk>
2018-12-08 20:09:00 +0000

    Add production url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 // Axios plugin
 const dev = 'http://localhost:5000'
-const production = ''
+const production = 'https://evanharry.pythonanywhere.com'
 axios.defaults.baseURL = (process.env.NODE_ENV !== 'production') ? dev : production
 
 let axiosPlugin = {}
